a0cacbd66b8da38636d1e4df0210a59957a37743
Kamil Kowalewski <kamil.kowalewski@nexontis.com>
2023-04-05 20:36:52 +0200

    Setting up axios with dotenv

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import * as dotenv from 'dotenv';
+//components
+import App from './components/App';
+//styles
+import './styles.scss';
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
