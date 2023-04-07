c6d8028f437c7dbfd14cacbb5e5266d6a8920a3d
arturrejment <arturrejment1@gmail.com>
2023-04-07 15:47:28 +0200

    serialize User, add register endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
+from .views import RegisterView
+
 urlpatterns = [
     path("token/obtain", TokenObtainPairView.as_view(), name="login"),
     path("token/verify", TokenVerifyView.as_view(), name="verify"),
     path("token/refresh", TokenRefreshView.as_view(), name="refresh"),
+    path("register", RegisterView.as_view(), name="register"),
 ]
