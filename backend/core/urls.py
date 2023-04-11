89dc402c40267d41a8912a355a9b4cb72cb64890
Patrycja Langkafel <patrycjalangkafel@gmail.com>
2023-04-11 15:45:41 +0200

    code generation

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
+from generate_codes.views import generate_codes_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,4 +13,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("generate_codes/<int:num_codes>/", generate_codes_view , name="generate_codes"),
 ]
