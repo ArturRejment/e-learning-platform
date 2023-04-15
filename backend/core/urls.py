45e2d335f5c06ed41c67c4e02bd45d60f6014615
arturrejment <arturrejment1@gmail.com>
2023-04-15 15:06:06 +0200

    create basic course model, view and serializer

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+
+from course.views import CourseViewSet
+
+router = DefaultRouter()
+router.register(r"course", CourseViewSet, basename="course")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,4 +17,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
