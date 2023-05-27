133a893adb7d86586e40c9e5404a9dca2af735da
arturrejment <arturrejment1@gmail.com>
2023-05-27 11:51:40 +0200

    create lesson model, extend admin panel, add lesson validation

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -30,3 +32,6 @@ urlpatterns = [
     ),
     path("", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/course/models.py
+++ backend/course/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -30,3 +31,36 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Lesson(models.Model):
+    class Type(models.TextChoices):
+        PDF = "pdf"
+        VIDEO = "video"
+
+    id = models.UUIDField(
+        verbose_name="id",
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    name = models.CharField(max_length=100)
+
+    lesson_type = models.CharField(choices=Type.choices, max_length=5)
+
+    youtube_video_url = models.URLField(null=True, blank=True)
+
+    pdf_file = models.FileField(upload_to="pdf", null=True, blank=True)
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.lesson_type == self.Type.PDF and not self.pdf_file:
+            raise ValidationError(
+                "Wbrano typ lekcji 'pdf', jednak nie podano pliku pdf."
+            )
+        if self.lesson_type == self.Type.VIDEO and not self.youtube_video_url:
+            raise ValidationError(
+                "Wbrano typ lekcji 'wideo', jednak nie podano linku do filmu."
+            )
