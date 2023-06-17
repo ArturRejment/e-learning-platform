fbc970b952479bc56fbe390bddc916ffc3a2f271
Kamil Kowalewski <kamil.kowalewski@nexontis.com>
2023-06-18 00:26:49 +0200

    Style Lesson

--- frontend/src/components/lesson/pdf/PdfViewer/PdfViewer.tsx
+++ frontend/src/components/lesson/pdf/PdfViewer/PdfViewer.tsx
@@ -3,6 +3,9 @@ import './PdfViewer.scss';
 import type { PDFDocumentProxy } from 'pdfjs-dist';
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import AutoSizer, { HorizontalSize } from 'react-virtualized-auto-sizer';
+
+import { Spinner } from '../../../utils';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -26,19 +29,25 @@ const PdfViewer = ({ file }: Props) => {
 
   return (
     <div className="pdf-viewer">
-      <Document
-        file={file}
-        onLoadSuccess={onDocumentLoadSuccess}
-        onContextMenu={(e) => e.preventDefault()}
-        className="pdf-viewer__document"
-      >
-        <Page
-          pageNumber={pageNumber}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-          className="pdf-viewer__page"
-        />
-      </Document>
+      <AutoSizer disableHeight>
+        {({ width }: HorizontalSize) => (
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onContextMenu={(e) => e.preventDefault()}
+            className="pdf-viewer__document"
+            loading={<Spinner />}
+          >
+            <Page
+              pageNumber={pageNumber}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              className="pdf-viewer__page"
+              width={width || 1}
+            />
+          </Document>
+        )}
+      </AutoSizer>
       <div className="pdf-viewer__buttons">
         <button
           className="pdf-viewer__button"
